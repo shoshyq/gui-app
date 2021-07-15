1e225a8e729730cebdd69f52cde66875c20045f2
bsd <bsd.odaya@gmail.com>
2021-07-15 18:42:10 +0300

    uploading

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,17 @@
-import { Component, Input, OnInit  } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
-
 })
 export class HeaderComponent implements OnInit {
-  @Input() bigger?: boolean = false;
 
+  @Input() bigger?: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}    
-
-
+}
