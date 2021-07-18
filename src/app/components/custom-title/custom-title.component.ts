1e9ff19a50c8449ee8e533824cd5614dfe9fca91
bsd <bsd.odaya@gmail.com>
2021-07-19 01:42:33 +0300

    upload subTitle

--- src/app/components/custom-title/custom-title.component.ts
+++ src/app/components/custom-title/custom-title.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CustomTitleComponent implements OnInit {
 
   @Input() text?: string = "";
+  @Input() subTitle?: string = "";
 
   constructor() { }
 
