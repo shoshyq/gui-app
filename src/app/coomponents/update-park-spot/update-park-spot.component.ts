cd325da16950ff330ca98a3d5de0c378e409ea73
Shoshy <tamid.lehajeh@gmail.com>
2021-07-19 23:23:17 +0300

    ok

---
+++ src/app/coomponents/update-park-spot/update-park-spot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-park-spot',
+  templateUrl: './update-park-spot.component.html',
+  styleUrls: ['./update-park-spot.component.css']
+})
+export class UpdateParkSpotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
