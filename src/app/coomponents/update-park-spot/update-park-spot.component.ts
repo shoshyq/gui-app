101a380b599e3c265cc8219e4b28a5f8b97ea753
bsd <bsd.odaya@gmail.com>
2021-07-20 01:56:51 +0300

    upload update park spot component

--- src/app/coomponents/update-park-spot/update-park-spot.component.ts
+++ src/app/coomponents/update-park-spot/update-park-spot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from 'src/app/models/city.models';
 
 @Component({
   selector: 'app-update-park-spot',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateParkSpotComponent implements OnInit {
 
+  cities: City[];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  onSelection(e) {
   }
 
 }
